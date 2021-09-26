d67a1bd6b148019daf053afafc3efcfebc44e6ac
Agustin Moran <agustin.moranr@gmail.com>
2021-09-25 19:19:06 -0500

    Exercise 2: useEffect hook: persistent state

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,31 +3,87 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+function handleLSExceptions(LSKey) {
+  if (!LSKey || typeof LSKey !== 'string' || LSKey === null) {
+    window.localStorage.removeItem(LSKey)
+    console.error('Key invalido: No se añaden elementos a local storage', {
+      keyValue: LSKey,
+    })
+    throw new Error('El valor del key no contiene un valor valido')
+  }
+}
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+function getValueFromLS(LSKey, LSDefaultValue, {deserialize}) {
+  handleLSExceptions(LSKey)
+
+  const valueInLS = window.localStorage.getItem(LSKey)
+
+  if (valueInLS) {
+    // the try/catch is here in case the localStorage value was set before
+    // we had the serialization in place (like we do in previous extra credits)
+    try {
+      return deserialize(valueInLS)
+    } catch (error) {
+      window.localStorage.removeItem(LSKey)
+    }
+  }
+  return typeof LSDefaultValue === 'function'
+    ? LSDefaultValue()
+    : LSDefaultValue
+}
+
+function useLocalStorageState(
+  LSKey = null,
+  LSDefaultValue = '',
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) {
+  const [currentValue, setCurrentValue] = React.useState(
+    getValueFromLS(LSKey, LSDefaultValue, {deserialize}),
+  )
+
+  const prevKeyRef = React.useRef(LSKey)
+
+  React.useEffect(() => {
+    handleLSExceptions(LSKey)
+    const prevKey = prevKeyRef.current
+
+    if (prevKey !== LSKey) {
+      window.localStorage.removeItem(prevKey)
+    }
+    prevKeyRef.current = LSKey
+
+    const valueToStore = serialize(currentValue)
+    window.localStorage.setItem(LSKey, valueToStore)
+  }, [LSKey, currentValue, serialize])
+
+  return [currentValue, setCurrentValue]
+}
+
+function Greeting({initialName = ''}) {
+  const [currentLSValue, setLSValue] = useLocalStorageState('name', initialName)
 
   function handleChange(event) {
-    setName(event.target.value)
+    let value = event.target.value
+    setLSValue(value)
   }
+
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
+        <input value={currentLSValue} onChange={handleChange} id="name" />
       </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+      {currentLSValue ? (
+        <strong>Hello {currentLSValue}</strong>
+      ) : (
+        'Please type your name'
+      )}
     </div>
   )
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Agustin Moran" />
 }
 
 export default App
